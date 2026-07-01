bcfe66866b46b5a32da2f82c65a0841a176f1654
AVØIID <huberjonathan@outlook.de>
2026-07-01 20:09:16 +0200

    Robot reworks

--- Assets/Scripts/State Machine/States/Enemy/Robot/RobotAttackState.cs
+++ Assets/Scripts/State Machine/States/Enemy/Robot/RobotAttackState.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using NUnit.Framework.Constraints;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,32 +9,47 @@ public class RobotAttackState : State
     private InventoryController _inventory;
     private NavMeshAgent _agent;
     private TransformVariable _targetTransform;
+    private GunBulletTracker _bulletTracker;
     private float _turnSpeed;
-    private bool _isFiringAShot;
+    private float _cooldownTimeAfterReload;
+    private float _cooldownTimePassed = 0f;
+    private bool _isCooldownActive = false;
+    private bool _isFiringAShot = false;
     public bool IsFiringAShot => _isFiringAShot;
+
     public RobotAttackState(
         NavMeshAgent agent,
         InventoryController inventory,
         TransformVariable targetTransform,
-        float turnSpeed
+        float turnSpeed,
+        TimeMS cooldownAfterReload
     )
     {
         _inventory = inventory;
         _turnSpeed = turnSpeed;
         _targetTransform = targetTransform;
         _agent = agent;
+        _cooldownTimeAfterReload = cooldownAfterReload.TotalSeconds;
     }
 
     public override void Enter()
     {
         Debug.Log($"[STATE MACHINE] Entering state: {this.GetType().Name.ToUpper()}.");
         _agent.updateRotation = false;
+
+        _isFiringAShot = false;
+
+        if (_inventory.ActiveItem is Weapon weapon)
+        {
+            _bulletTracker = weapon.gameObject.GetComponent<GunBulletTracker>();
+        }
     }
 
     public override void Exit()
     {
         Debug.Log($"[STATE MACHINE] Exiting state: {this.GetType().Name.ToUpper()}.");
         _agent.updateRotation = true;
+        _isFiringAShot = false;
     }
 
     public override void Run(float deltaTime)
@@ -49,8 +66,33 @@ public class RobotAttackState : State
             false
         );
 
+        if (_isCooldownActive) 
+            _cooldownTimePassed += deltaTime;
+        
+        if (_cooldownTimePassed >= _cooldownTimeAfterReload)
+        {
+            _isCooldownActive = false;
+            _cooldownTimePassed = 0.0f;
+        }
+
+        if (_isCooldownActive)
+            return;
+        
         bool itemUseSuccessful = _inventory.UseActiveItem(itemUsageData);
-        _isFiringAShot = itemUseSuccessful;
+        _isFiringAShot = itemUseSuccessful; 
+
+        if (_bulletTracker == null)
+            return;
+        
+        if (!_bulletTracker.HasBulletsLeft())
+        {
+            _isFiringAShot = false;
+
+            Weapon weapon = _inventory.ActiveItem as Weapon;
+            weapon.Reload();
+
+            _isCooldownActive = true;
+        }
     }
 
     private void RotateToTarget(float turnSpeed, float deltaTime)
@@ -60,10 +102,10 @@ public class RobotAttackState : State
 
         Quaternion currentRotation = _agent.transform.rotation;
         Quaternion targetRotation = Quaternion.LookRotation(directionToTarget.normalized);
-        
+
         _agent.transform.rotation = Quaternion.Lerp(
-            currentRotation, 
-            targetRotation, 
+            currentRotation,
+            targetRotation,
             deltaTime * turnSpeed
         );
     }
