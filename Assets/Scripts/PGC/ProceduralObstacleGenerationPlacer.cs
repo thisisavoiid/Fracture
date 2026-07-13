79d490f545b39c15a6087ad8ddb2df0276202b3c
AVØIID <huberjonathan@outlook.de>
2026-07-13 02:36:58 +0200

    Small bugfixes

--- Assets/Scripts/PGC/ProceduralObstacleGenerationPlacer.cs
+++ Assets/Scripts/PGC/ProceduralObstacleGenerationPlacer.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NaughtyAttributes;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.Rendering.VirtualTexturing;
 using Random = UnityEngine.Random;
